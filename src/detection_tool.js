1860d73db376cee55dcc97d566e39bc1f932b06c
jlucidar <jlucidar@gmail.com>
2014-07-02 19:50:32 -0400

    make it windwos compatible.
    there is no other way to do it in a better way, due to the network layer
    implementation in windows...

--- src/detection_tool.js
+++ src/detection_tool.js
@@ -2,18 +2,45 @@ var os=require('os');
 var util=require('util');
 var EventEmitter = require('events').EventEmitter;
 var dgram = require('dgram');
+var os = require('os');
 
 var OK = "YES I M !\0";
 var ERR = "I DNT UNDRSTND !\0";
 var HOSTNAME = "U NAME ?\0";
 var REQ = "R U A SBT ?\0";
-
+var socket;
 var broadcastAddress = '255.255.255.255';
 var broadcastPort = 24862; // = 7777 without conversion
 
+//ennoying funcction but no other way to deal with it in windows
+function send_broadcast_to_every_interfaces(){
+	Object.keys(os.networkInterfaces()).forEach(function(key,index,arr){ //val = ip adresses , key = name of interface
+		var networks_list = this;
+		networks_list[key].forEach(function(val2,key2,arr2){
+			if (val2.internal === false && val2.family === 'IPv4')
+			{
+				if (val2.address.split('.')[0] >= 192 &&  val2.address.split('.')[0] <= 223){ //class C
+					broadcastAddress = val2.address.split('.')[0] +'.'+ val2.address.split('.')[1] +'.'+ val2.address.split('.')[2] + '.255';
+				}
+				else if (val2.address.split('.')[0] >= 128 &&  val2.address.split('.')[0] <= 191){ //class B 
+					broadcastAddress = val2.address.split('.')[0]  +'.'+ val2.address.split('.')[1] + '.255.255';
+				}
+				else if (val2.address.split('.')[0] >= 1 &&  val2.address.split('.')[0] <= 126) { //class A
+					broadcastAddress = val2.address.split('.')[0] + '.255.255.255';
+				}
+				socket.send(new Buffer(REQ), 0, REQ.length, broadcastPort, broadcastAddress, function (err) {
+						if (err) console.log(err);
+				});
+			}
+		});
+	},os.networkInterfaces());
+	socket.send(new Buffer(REQ), 0, REQ.length, broadcastPort,'192.168.10.7', function (err) { //usb0
+		if (err) console.log(err);
+		});
+}
 
 var detection = function(t) {
-	var socket = dgram.createSocket('udp4');
+	socket = dgram.createSocket('udp4');
 	var current_dialog =[];
 	var device;
 	var devices =[];
@@ -21,7 +48,8 @@ var detection = function(t) {
 	var timeout = t || 1000;
 	var that =this;
 	socket.bind(function(){
-		socket.setBroadcast(true);});
+		socket.setBroadcast(true);
+	});
 
 		socket.on("message", function ( data, rinfo ) {
 			if(data.toString() == OK)
@@ -47,11 +75,14 @@ var detection = function(t) {
 				console.log("[detection_tool.js] received from "+rinfo.address+" : unknow message : '"+ data.toString() +"'");
 			}
 		});
-
-		socket.send(new Buffer(REQ), 0, REQ.length, broadcastPort, broadcastAddress, function (err) {
+		if(os.platform()==='win32'){
+			send_broadcast_to_every_interfaces();
+		}
+		else{
+			socket.send(new Buffer(REQ), 0, REQ.length, broadcastPort, broadcastAddress, function (err) {
 						if (err) console.log(err);
-						//console.log("Broadcast message sent");
 					});
+		}
 
 
 
