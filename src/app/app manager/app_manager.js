20ff87399a50eae35d8a6ca21c8629af7f47d3d4
jlucidar <jlucidar@gmail.com>
2014-07-13 22:50:58 -0400

    finish the app manager

--- src/app/app manager/app_manager.js
+++ src/app/app manager/app_manager.js
@@ -1,11 +1,28 @@
-//var zip = require('unzip');
-//var fs = require('fs');
+var zip = require('adm-zip');
+var path = require('path');
 
-process.on('exit', function(code) {
-	fs.rmdirSync('~/Desktop/tmp/file_execute');
+
+/*need the path of the application to load (.fma)*/
+function load_app(app_path,callback){
+	decompress_app(app_path,function(index_page){
+		global.app_loaded=index_page;
+		callback('./app_executer.html');
+	});
+
 }
 
-fs.createReadStream('~/Desktop/file_executer.fma').pipe(zip.Extract({ path: '~/Desktop/tmp/file_execute' }));
-
-
-
+/* decompress the app and return the temporary link of the home page*/
+function decompress_app(app_path,callback){
+	var tmp_app_path="./tmp/app/"+path.basename(app_path)+"/";
+	var app = new zip(app_path);
+	console.log(app);
+	app.extractAllTo(/*target path*/tmp_app_path, /*overwrite*/true);
+	try{
+		var package_info = require(tmp_app_path+'package.json');
+		callback('../../'+path.join(tmp_app_path,package_info.main));
+	}
+	catch(ex){
+		console.log(ex);
+	}
+	
+}
