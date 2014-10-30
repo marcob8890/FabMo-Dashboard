54a75144054f082e7f64c311a7cc6fa33338c0ad
Ryan Sturmer <ryansturmer@gmail.com>
2014-10-30 08:59:48 -0400

    Minor docs changes

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,15 +1,15 @@
-/*
- * app.js
- * ------
- * 
- * app.js is the entry point of the application.  It configures require.js for including javascript modules
- * and launches the main script file which is main.js
+/* 
+ * app.js is the application bootstrap script.  It configures require.js for including javascript modules
+ * and launches the application entry point file which is main.js
  */
 requirejs.config({
     baseUrl: 'js'
 });
 
+// require.js plays havoc with node-webkit.  After include require.js, we actually
+// patch up the node-webkit built-in require, and just use the `requirejs` namespace for accessing
+// our application entry point.
 window.require = window.requireNode;
 
-// Single entry point for require program
+// Run the main application!
 requirejs(['main']);
